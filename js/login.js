d3888c9f22eb9fa7fbbb5413cce49ec8a3c9abdb
Imandra <shalyapin.alexej@yandex.ru>
2016-11-24 15:29:39 +0400

    add scripts

---
+++ js/login.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('#sign-up').on('submit', function (event) {
+        event.preventDefault();
+        var form = $(this);
+        var formData = form.serialize();
+
+        $.ajax({
+            url: form.attr('action'),
+            type: form.attr('method'),
+            data: formData,
+            dataType: 'text',
+            success: function (data) {
+                if (data) {
+                    $('#result').html(data);
+                }
+            },
+            error: function (xhr) {
+                $('#result').text('Ошибка, код состояния HTTP: ' +
+                    xhr.status + ' ' + xhr.statusText);
+            }
+        });
+    });
+});
