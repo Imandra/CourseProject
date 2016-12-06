1bc9c59f408615a4c3712c2323806c64825e3506
Imandra <shalyapin.alexej@yandex.ru>
2016-12-07 00:51:45 +0400

    reformat login.js

--- js/login.js
+++ js/login.js
@@ -3,21 +3,24 @@ $(function () {
         event.preventDefault();
         var form = $(this);
         var formData = form.serialize();
-
         $.ajax({
             url: form.attr('action'),
             type: form.attr('method'),
             data: formData,
             dataType: 'text',
-            success: function (data) {
-                if (data) {
-                    $('#result').html(data);
+            success: [
+                function (data) {
+                    if (data) {
+                        $('#result').html(data);
+                    }
                 }
-            },
-            error: function (xhr) {
-                $('#result').text('Ошибка, код состояния HTTP: ' +
-                    xhr.status + ' ' + xhr.statusText);
-            }
+            ],
+            error: [
+                function (xhr) {
+                    $('#result').text('Ошибка, код состояния HTTP: ' +
+                        xhr.status + ' ' + xhr.statusText);
+                }
+            ]
         });
     });
 });
